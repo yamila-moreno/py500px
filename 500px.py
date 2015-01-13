932acdc1274068a10ab8301fb1cdf2d396a23a9f
Yamila Moreno <yamila.ms@gmail.com>
2015-01-13 23:32:55 +0100

    Add photo uploading from url

--- 500px.py
+++ 500px.py
@@ -8,8 +8,9 @@ import json
 
 # ENDPOINTS
 COLLECTIONS_URL = ENDPOINT + 'collections/'
-COLLECTION_URL = ENDPOINT + 'collections/{col_id}'
-PHOTO_URL = ENDPOINT, 'photos/{photo_id}'
+COLLECTION_URL = ENDPOINT + 'collections/{col_id}/'
+PHOTO_URL = ENDPOINT, 'photos/{photo_id}/'
+PHOTO_UPLOAD_URL = ENDPOINT + 'photos/upload/'
 ADD_TAG_TO_PHOTO_URL = ENDPOINT + 'photos/{photo_id}/tags?tags={tag}'
 SET_NAME_TO_PHOTO_URL = ENDPOINT + 'photos/{photo_id}/?name={name}'
 
@@ -34,14 +35,33 @@ def set_tag_for_collection(oauth, col_id, tag):
         r = oauth.post(ADD_TAG_TO_PHOTO_URL.format(photo_id=photo['id'], tag=tag))
     print('ok!')
 
+def create_collection(oauth, name, path):
+    url = COLLECTIONS_URL + '?kind=2&title={}&path={}'.format(name, path)
+    r = oauth.post(url)
+    print(json.loads(r.text)['id'])
+    print('ok!')
+    return(json.loads(r.text))
+
+def upload_photo_from_url(oauth, photo_url, name):
+    files = {
+        'file': requests.get(url).content
+        }
+    url = PHOTO_UPLOAD_URL + '?name=' + name
+    r = oauth.post(url, files=files)
+    print('ok!')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--action', help='Action',
-                        choices=['collections', 'set_tag', 'set_name'])
+                        choices=['collections', 'set_tag',
+                                 'set_name', 'create_collection',
+                                 'upload_photo'])
     parser.add_argument('--col_id', help='Collection id')
     parser.add_argument('--tag', help='Tag')
     parser.add_argument('--name', help='Name')
+    parser.add_argument('--path', help='Path or slug')
+    parser.add_argument('--photo_url', help='Photo url')
     args = parser.parse_args()
 
     oauth = OAuth1Session(KEY,
@@ -62,4 +82,11 @@ if __name__ == '__main__':
         name = args.name
         set_name_for_photos_in_a_collection(oauth, col_id, name)
 
+    elif args.action == 'create_collection':
+        name = args.name
+        path = args.path
+        create_collection(oauth, name, path)
+
+    elif args.action == 'upload_photo':
+        upload_photo(oauth, '', 'kitten')
 
